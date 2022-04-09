d34da80aee02950b27bc9a87037d2e877029771a
Tubanur Balci <tubanurbalci@gmail.com>
2022-04-09 22:39:48 +0300

    MultiThreads

---
+++ Runnable/Counter.java
@@ -0,0 +1,24 @@
+public class Counter implements Runnable{
+    private String name;
+
+    public Counter(String name){
+        this.name=name;
+    }
+
+    @Override
+    public void run() {
+       System.out.println(this.getName()+ " sayacı başladı !");
+       for (int i = 0; i < 10; i++) {
+           System.out.println(this.getName()+": "+ i);
+       }
+        
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
